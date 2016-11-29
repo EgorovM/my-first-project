efa43a4779b3c61a47383e4b9b5fc2513e7d34d9
EgorovM <egorov_michil@mail.ru>
2016-11-29 17:22:20 +0900

    first commit

--- help/models.py
+++ help/models.py
@@ -1,22 +1,21 @@
 from __future__ import unicode_literals
-from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
-import datetime
 
+@python_2_unicode_compatible
 class Order(models.Model):
-    
-    address = models.CharField(max_length = 20)
     problem = models.CharField(max_length=30)
-    telephone = models.CharField(max_length = 10)    
+    telephone = models.CharField(max_length = 10)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.telephone
 
+@python_2_unicode_compatible
 class Comment(models.Model):
-
     order_info = models.ForeignKey(Order)
     comment_text = models.CharField(max_length = 30)
-    
+
     def __str__(self):
         return self.comment_text
+
--- help/views.py
+++ help/views.py
@@ -3,36 +3,34 @@ from .models import Order, Comment
 from django.shortcuts import render
 from django.utils import timezone
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 def index(request):
     if request.method == "POST":
         if "ok_button" in request.POST:
             telephone = request.POST["telephone"]
             problem   = request.POST["problem"]
-            address   = request.POST["address"]
 
             orr = Order()
-            
+
             orr.telephone = telephone
             orr.problem   = problem
-            orr.address   = address
             orr.pub_date  = timezone.now()
             orr.save()
 
     latest_order_list = Order.objects.all()[::-1]
-    paginator = Paginator(latest_order_list,2)
+
+    paginator = Paginator(latest_order_list,3)
 
     page = request.GET.get('page')
     try:
         latest_order_list = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
         latest_order_list = paginator.page(1)
     except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
         latest_order_list = paginator.page(paginator.num_pages)
 
-    context = {"latest_order_list":latest_order_list}
-    
+    context = {"latest_order_list":latest_order_list,"paginator":paginator,}
+
     return render(request, "help/index.html",context)
 
 def order(request, order_id):
@@ -43,14 +41,15 @@ def order(request, order_id):
 
             cmt              = Comment()
 
-            cmt.order_info   = orr 
+            cmt.order_info   = orr
             cmt.comment_text = comment_text
             cmt.save()
 
     latest_cmt_list = Comment.objects.all()[::-1]
     order = Order.objects.get(id = order_id)
-    
+
     contex = {"latest_cmt_list":latest_cmt_list,"order":order,}
 
     return render(request, "help/order.html", contex)
+
 
